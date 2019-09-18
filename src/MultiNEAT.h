6e66e2714454822194bf50c47cee599b0f5a6a1c
Matteo De Carlo <matteo.dek@gmail.com>
2019-09-18 12:14:30 +0200

    Fixed master include

--- src/MultiNEAT.h
+++ src/MultiNEAT.h
@@ -5,18 +5,18 @@
 #ifndef MULTINEAT_MULTINEAT_H
 #define MULTINEAT_MULTINEAT_H
 
-#include <MultiNEATAssert.h>
-#include <Genes.h>
-#include <Genome.h>
-#include <Innovation.h>
-#include <NeuralNetwork.h>
-#include <Parameters.h>
-#include <PhenotypeBehavior.h>
-#include <Population.h>
-#include <Random.h>
-#include <Species.h>
-#include <Substrate.h>
-#include <Traits.h>
-#include <Utils.h>
+#include "MultiNEATAssert.h"
+#include "Genes.h"
+#include "Genome.h"
+#include "Innovation.h"
+#include "NeuralNetwork.h"
+#include "Parameters.h"
+#include "PhenotypeBehavior.h"
+#include "Population.h"
+#include "Random.h"
+#include "Species.h"
+#include "Substrate.h"
+#include "Traits.h"
+#include "Utils.h"
 
 #endif //MULTINEAT_MULTINEAT_H
