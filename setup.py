7b6882fba34c64d6330e4b402a2d4dbdf9ea0b8f
Matteo De Carlo <matteo.dek@gmail.com>
2019-10-11 20:57:35 +0200

    pip install in release mode

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ from skbuild import setup
 setup(name='multineat',
       version='0.6',  # Update version in conda/meta.yaml as well
       packages=['multineat'],
-      cmake_args=['-DGENERATE_PYTHON_BINDINGS:BOOL=ON'],
-      install_requires=[
-            'numpy',
-      ],
+      cmake_args=['-DGENERATE_PYTHON_BINDINGS:BOOL=ON', '-DCMAKE_BUILD_TYPE=Release'],
+      install_requires=['numpy'],
       )
